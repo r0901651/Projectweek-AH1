e4555d4f40a35df79e596140fdafc25510c720b3
MichielKuyken <r0938842@student.thomasmore.be>
2024-01-24 11:08:34 +0100

    rfid file

--- rfid.py
+++ rfid.py
@@ -4,10 +4,9 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-        text = input('New data:')
-        print("Now place your tag to write")
-        reader.write(text)
-        print("Written")
+        id, text = reader.read()
+        print(id)
+        print(text)
 
 finally:
         GPIO.cleanup()
