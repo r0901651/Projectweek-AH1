77aa662c1d400fce923e1015b88cb15e7e8c7e75
r0901651 <r0901651@student.thomasmore.be>
2024-01-25 12:07:57 +0100

    "added docker support"

--- fastapi/myproject/database.py
+++ fastapi/myproject/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlitedb/sqlitedata.db"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://admin-examens:7EYXRjS9z2Ya62MYqy3hZCTf@examen-mariadb/examens"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, pool_pre_ping=True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
