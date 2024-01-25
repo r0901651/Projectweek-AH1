0838361c4ff92cfa7190ccf409738dc510f91bca
r0901651 <r0901651@student.thomasmore.be>
2024-01-25 15:25:37 +0100

    "Changed mariadb to sqlite"

--- fastapi/myproject/database.py
+++ fastapi/myproject/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://admin-examens:7EYXRjS9z2Ya62MYqy3hZCTf@examen-mariadb/examens"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlitedb/sqlitedata.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, pool_pre_ping=True
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
